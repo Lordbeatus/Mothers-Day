9d31a93f3b194b2deb63c0654d44de19ef4fdf2a
Lordbeatus <ellipsesking@gmail.com>
2025-05-10 21:33:43 -0700

    here

---
+++ Mothers-Day/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all your React components
+    "./public/index.html",        // Include your HTML file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
--- Mothers-Day/webpack.config.js
+++ Mothers-Day/webpack.config.js
@@ -1,38 +1,28 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.jsx',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        compress: true,
-        port: 3000,
-        historyApiFallback: true
-    },
-    mode: 'development'
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'], // Add postcss-loader
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    static: './public',
+  },
 };
